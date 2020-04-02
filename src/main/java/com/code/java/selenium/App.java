5db817ac139ed6cfb7fbad003d29d40bbe72de3a
Venky <venkatesh4a31@gmail.com>
2020-04-02 17:56:26 +0530

    Initial commit

---
+++ src/main/java/com/code/java/selenium/App.java
@@ -0,0 +1,16 @@
+package com.code.java.selenium;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test()
+    public static void main1( )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
